1e80f5cf1a5a0965728c96f4dddf1ccb5ed2b240
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-01-24 19:54:18 +0530

    add transformer tests

---
+++ tests/test_transformer.py
@@ -0,0 +1,13 @@
+﻿import unittest
+from transformers.data_transformer import LogTransformer
+from utils.logger import setup_production_logging
+class TestTransformer(unittest.TestCase):
+    def setUp(self):
+        self.logger = setup_production_logging()
+        self.transformer = LogTransformer(self.logger)
+    def test_payload_transformation(self):
+        mock_raw = [{"event_id": "evt_123", "action": "click"}]
+        res = self.transformer.transform_payload(mock_raw)
+        self.assertEqual(res[0]["action"], "CLICK")
+if __name__ == '__main__':
+    unittest.main()
