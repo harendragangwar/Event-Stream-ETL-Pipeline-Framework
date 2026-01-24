fe5091e44aa4d3036c204ffd39febdfde2b05416
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-01-24 14:35:10 +0530

    add extractor tests

---
+++ tests/test_extractor.py
@@ -0,0 +1,12 @@
+﻿import unittest
+from extractors.log_extractor import RawLogExtractor
+from utils.logger import setup_production_logging
+class TestExtractor(unittest.TestCase):
+    def setUp(self):
+        self.logger = setup_production_logging()
+        self.extractor = RawLogExtractor(self.logger)
+    def test_extraction_count(self):
+        data = self.extractor.extract_raw_logs()
+        self.assertEqual(len(data), 10)
+if __name__ == '__main__':
+    unittest.main()
