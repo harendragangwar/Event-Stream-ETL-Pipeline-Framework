0dabd407d13201f42f943f1f0b1c0e2886ac5042
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-01-29 15:23:41 +0530

    tweak logging format

--- utils/logger.py
+++ utils/logger.py
@@ -1,12 +1,11 @@
-import os
+﻿import os
 import sys
 import logging
-
 def setup_production_logging():
     log_dir = "logs"
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
-    log_format = "%(asctime)s [%(levelname)s] - %(message)s"
+    log_format = "%(asctime)s [%(levelname)s] (ETL) - %(message)s"
     logging.basicConfig(
         level=logging.INFO,
         format=log_format,
