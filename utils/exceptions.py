59d2629393d61c974337f09570b303f6cf7d4275
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-01-31 16:29:43 +0530

    add load exceptions

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,3 +1,4 @@
 ﻿class PipelineException(Exception): pass
 class DataExtractionError(PipelineException): pass
 class DataTransformationError(PipelineException): pass
+class StorageLoadError(PipelineException): pass
