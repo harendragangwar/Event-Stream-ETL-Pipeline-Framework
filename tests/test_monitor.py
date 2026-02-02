399e42b6791b2926fcf72b64c8d86487af3b5b38
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-02-02 14:12:09 +0530

    add monitor tests

---
+++ tests/test_monitor.py
@@ -0,0 +1,12 @@
+﻿import unittest
+from utils.logger import setup_production_logging
+from utils.system_monitor import PipelineSystemMonitor
+class TestSystemMonitor(unittest.TestCase):
+    def setUp(self):
+        self.logger = setup_production_logging()
+        self.monitor = PipelineSystemMonitor(self.logger)
+    def test_telemetry_execution(self):
+        res = self.monitor.collect_memory_usage()
+        self.assertIsInstance(res, float)
+if __name__ == '__main__':
+    unittest.main()
