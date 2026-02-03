e3d0c47473d90cdabe499f902a020e807b933672
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-02-03 19:21:45 +0530

    add sanitizer tests

---
+++ tests/test_sanitizer.py
@@ -0,0 +1,9 @@
+﻿import unittest
+from utils.sanitizer import DataSanitizer
+class TestSanitizer(unittest.TestCase):
+    def test_clean_string(self):
+        dirty_input = "CLICK!!!123"
+        res = DataSanitizer.clean_alphanumeric(dirty_input)
+        self.assertEqual(res, "CLICK123")
+if __name__ == '__main__':
+    unittest.main()
