134f93b412a29fb675f1654890856806878f7c18
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-02-03 10:14:32 +0530

    add text sanitizer

---
+++ utils/sanitizer.py
@@ -0,0 +1,7 @@
+﻿import re
+class DataSanitizer:
+    @staticmethod
+    def clean_alphanumeric(text):
+        if not text:
+            return ""
+        return re.sub(r'[^a-zA-Z0-9_\-\s]', '', str(text))
