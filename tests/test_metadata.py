848113926845b27eb06eb03b4d31961d2a5d791f
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-02-06 10:14:11 +0530

    add metadata tests

---
+++ tests/test_metadata.py
@@ -0,0 +1,12 @@
+﻿import unittest
+import os
+from utils.logger import setup_production_logging
+from utils.metadata_manager import PipelineMetadataTracker
+class TestMetadata(unittest.TestCase):
+    def setUp(self):
+        self.logger = setup_production_logging()
+        self.tracker = PipelineMetadataTracker(self.logger)
+    def test_summary_payload(self):
+        self.assertIsNotNone(self.tracker)
+if __name__ == '__main__':
+    unittest.main()
