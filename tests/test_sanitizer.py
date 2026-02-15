318c68e8558668daaac4453a5620cb9fb8229c4e
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-02-15 09:41:12 +0530

    update sanitizer tests

--- tests/test_sanitizer.py
+++ tests/test_sanitizer.py
@@ -2,8 +2,8 @@
 from utils.sanitizer import DataSanitizer
 class TestSanitizer(unittest.TestCase):
     def test_clean_string(self):
-        dirty_input = "CLICK!!!123"
+        dirty_input = "  CLICK!!!123   CORE  "
         res = DataSanitizer.clean_alphanumeric(dirty_input)
-        self.assertEqual(res, "CLICK123")
+        self.assertEqual(res, "CLICK123 CORE")
 if __name__ == '__main__':
     unittest.main()
