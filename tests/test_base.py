c4a2f71bf7d785e34a442c0503d07a570780ed8f
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-02-18 19:05:44 +0530

    clean up asserts

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,6 @@
 ﻿import unittest
 class TestPipelineBase(unittest.TestCase):
     def test_initial_state(self):
-        self.assertEqual(1, 1)
+        self.assertTrue(1 == 1)
 if __name__ == '__main__':
     unittest.main()
