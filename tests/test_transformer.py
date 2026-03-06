d226a3a6ff5b2e3d745de4744617c47ad5b90814
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-03-06 15:24:11 +0530

    sync transformer test profile with session validation structures

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -6,9 +6,10 @@ class TestTransformer(unittest.TestCase):
         self.logger = setup_production_logging()
         self.transformer = LogTransformer(self.logger)
     def test_payload_transformation(self):
-        mock_raw = [{"event_id": "evt_123", "action": "click", "device_type": "MOBILE", "user_id": "usr_999"}]
+        mock_raw = [{"event_id": "evt_123", "action": "click", "device_type": "MOBILE", "session_duration_sec": "500"}]
         res = self.transformer.transform_payload(mock_raw)
         self.assertEqual(res[0]["action"], "CLICK")
         self.assertEqual(res[0]["device_type"], "mobile")
+        self.assertEqual(res[0]["session_duration_sec"], 500)
 if __name__ == '__main__':
     unittest.main()
