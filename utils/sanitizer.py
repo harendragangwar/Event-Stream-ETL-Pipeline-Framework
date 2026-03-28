1188cae8f56d36d37a4e3860e440d58c1bb92f09
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-03-28 14:55:02 +0530

    extend sanitizer guards

--- utils/sanitizer.py
+++ utils/sanitizer.py
@@ -7,3 +7,8 @@ class DataSanitizer:
         safe_text = str(text).encode('ascii', 'ignore').decode('ascii')
         cleaned = re.sub(r'[^a-zA-Z0-9_\-\s]', '', safe_text)
         return " ".join(cleaned.split())
+    @staticmethod
+    def is_valid_uuid(token):
+        if not token:
+            return False
+        return bool(re.match(r'^evt_[a-zA-Z0-9_\-]+$', str(token)))
