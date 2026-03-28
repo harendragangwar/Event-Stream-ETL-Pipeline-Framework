4fe596b03cbfbef409b0fba9d0df560108dcc83b
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-03-28 18:19:44 +0530

    sync sanitizer tests

--- tests/test_sanitizer.py
+++ tests/test_sanitizer.py
@@ -5,9 +5,8 @@ class TestSanitizer(unittest.TestCase):
         dirty_input = "  CLICK!!!123   CORE  "
         res = DataSanitizer.clean_alphanumeric(dirty_input)
         self.assertEqual(res, "CLICK123 CORE")
-    def test_unicode_filtering(self):
-        unicode_input = "PURCHASE_航空"
-        res = DataSanitizer.clean_alphanumeric(unicode_input)
-        self.assertEqual(res, "PURCHASE_")
+    def test_token_validation(self):
+        self.assertTrue(DataSanitizer.is_valid_uuid("evt_12345"))
+        self.assertFalse(DataSanitizer.is_valid_uuid("invalid_token"))
 if __name__ == '__main__':
     unittest.main()
