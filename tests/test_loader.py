ffdc6860647b818f2599eb0272f1a8b1c0fb9d00
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-03-29 14:05:31 +0530

    patch structural loader tests

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -2,15 +2,15 @@
 import os
 from loaders.data_loader import DiskDataLoader
 from utils.logger import setup_production_logging
+
 class TestLoader(unittest.TestCase):
     def setUp(self):
         self.logger = setup_production_logging()
         self.config = {"raw_stage_path": "data/raw", "processed_stage_path": "data/processed"}
         self.loader = DiskDataLoader(self.logger, self.config)
-    def test_loader_instance(self):
-        self.assertIsNotNone(self.loader)
-    def test_sync_verification_fallback(self):
-        res = self.loader.verify_load_sync("non_existent_id_000")
+
+    def test_verify_load_sync_nonexistent(self):
+        res = self.loader.verify_load_sync("null_token_id")
         self.assertFalse(res)
 if __name__ == '__main__':
     unittest.main()
