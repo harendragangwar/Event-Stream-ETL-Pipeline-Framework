d505ab2c24c5182132bbed76c8352a852769a43a
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-03-29 19:24:55 +0530

    clean assertion paths

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,6 @@
 ﻿import unittest
 class TestPipelineBase(unittest.TestCase):
     def test_initial_state(self):
-        self.assertTrue(1 == 1)
+        self.assertTrue(True)
 if __name__ == '__main__':
     unittest.main()
