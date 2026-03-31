fd247587f885ffc68b524299bf15af74ff8dacfd
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-03-31 16:54:11 +0530

    sync truncation test limits

--- tests/test_sanitizer.py
+++ tests/test_sanitizer.py
@@ -1,12 +1,14 @@
 ﻿import unittest
 from utils.sanitizer import DataSanitizer
+
 class TestSanitizer(unittest.TestCase):
     def test_clean_string(self):
         dirty_input = "  CLICK!!!123   CORE  "
         res = DataSanitizer.clean_alphanumeric(dirty_input)
         self.assertEqual(res, "CLICK123 CORE")
-    def test_token_validation(self):
-        self.assertTrue(DataSanitizer.is_valid_uuid("evt_12345"))
-        self.assertFalse(DataSanitizer.is_valid_uuid("invalid_token"))
-if __name__ == '__main__':
-    unittest.main()
+
+    def test_string_truncation_limits(self):
+        long_input = "ACTION_" + "X" * 100
+        res = DataSanitizer.enforce_string_limits(long_input, max_len=20)
+        self.assertEqual(len(res), 20)
+        self.assertTrue(res.startswith("ACTION_"))
