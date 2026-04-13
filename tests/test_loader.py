4d0d4595a5cd4ec2bec87d5d45bef02cae7f9cc8
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-04-13 16:12:44 +0530

    sync loader threshold tests

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -6,11 +6,19 @@ from utils.logger import setup_production_logging
 class TestLoader(unittest.TestCase):
     def setUp(self):
         self.logger = setup_production_logging()
-        self.config = {"raw_stage_path": "data/raw", "processed_stage_path": "data/processed"}
+        self.config = {
+            "raw_stage_path": "data/raw", 
+            "processed_stage_path": "data/processed",
+            "cleanup_threshold": 85.0
+        }
         self.loader = DiskDataLoader(self.logger, self.config)
 
-    def test_verify_load_sync_nonexistent(self):
-        res = self.loader.verify_load_sync("null_token_id")
+    def test_loader_instance_with_thresholds(self):
+        self.assertIsNotNone(self.loader)
+        self.assertEqual(self.loader.config["cleanup_threshold"], 85.0)
+
+    def test_sync_verification_fallback(self):
+        res = self.loader.verify_load_sync("non_existent_id_2026")
         self.assertFalse(res)
 if __name__ == '__main__':
     unittest.main()
