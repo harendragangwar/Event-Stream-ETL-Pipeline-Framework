49fdb641a6ab1fc7375ee52fe1f8208ec5969ac3
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-04-24 15:23:44 +0530

    sync stream partition tests

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -13,6 +13,11 @@ class TestTransformer(unittest.TestCase):
         self.assertEqual(res[0]["action"], "CLICK")
         self.assertEqual(res[0]["device_type"], "mobile")
 
+    def test_stream_partition_key_assignment(self):
+        mock_raw = [{"event_id": "evt_777", "action": "purchase", "device_type": "tablet", "extraction_thread_id": 3}]
+        res = self.transformer.transform_payload(mock_raw)
+        self.assertEqual(res[0]["partition_key"], "thread_3")
+
     def test_string_limit_truncation(self):
         long_action = "BURST_ACTION_" + "X" * 100
         mock_raw = [{"event_id": "evt_999", "action": long_action, "device_type": "desktop"}]
