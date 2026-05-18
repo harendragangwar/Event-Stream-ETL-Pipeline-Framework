46c9ef30d67149a7754fe6544116364ac94c0d74
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-05-18 15:23:44 +0530

    sync attempt index tests

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -10,19 +10,18 @@ class TestTransformer(unittest.TestCase):
     def test_payload_transformation_bounds(self):
         mock_raw = [{"event_id": "evt_123", "action": "click", "device_type": "MOBILE", "session_duration_sec": "500"}]
         res = self.transformer.transform_payload(mock_raw)
-        self.assertEqual(res["action"], "CLICK")
-        self.assertEqual(res["device_type"], "mobile")
+        self.assertEqual(res[0]["action"], "CLICK")
+        self.assertEqual(res[0]["device_type"], "mobile")
+
+    def test_attempt_index_mapping(self):
+        mock_raw = [{"event_id": "evt_555", "action": "view", "extraction_attempt": 2}]
+        res = self.transformer.transform_payload(mock_raw)
+        self.assertEqual(res[0]["attempt_index"], 2)
 
     def test_injection_safe_payload_transform(self):
         mock_raw = [{"event_id": "evt_456", "action": "purchase; DROP TABLE users; --", "device_type": "desktop"}]
         res = self.transformer.transform_payload(mock_raw)
-        self.assertNotIn(";", res["action"])
-        self.assertNotIn("-", res["action"])
-        self.assertTrue(res["action"].startswith("PURCHASE DROP"))
-
-    def test_stream_partition_key_assignment(self):
-        mock_raw = [{"event_id": "evt_777", "action": "purchase", "device_type": "tablet", "extraction_thread_id": 3}]
-        res = self.transformer.transform_payload(mock_raw)
-        self.assertEqual(res["partition_key"], "thread_3")
+        self.assertNotIn(";", res[0]["action"])
+        self.assertTrue(res[0]["action"].startswith("PURCHASE DROP"))
 if __name__ == '__main__':
     unittest.main()
