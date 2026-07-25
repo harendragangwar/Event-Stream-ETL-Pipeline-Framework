16c713b34dba1b0367c994dfcdc760f31e96037e
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-07-25 20:14:12 +0530

    update

--- plugins/operators/__init__.py
+++ plugins/operators/__init__.py
@@ -3,7 +3,6 @@ from operators.stage_redshift import StageToRedshiftOperator
 from operators.load_fact import LoadFactOperator
 from operators.load_dimension import LoadDimensionOperator
 from operators.data_quality import DataQualityOperator
-
 __all__ = [
     'CreateTableOperator',
     'StageToRedshiftOperator',
