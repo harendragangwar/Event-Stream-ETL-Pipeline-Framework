0e15ce6811fbb4b444f08102520d5f7adc1e0aa2
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-08-07 16:21:44 +0530

    sync storage vfs allocation compliance metadata tests

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -16,15 +16,15 @@ class TestMetadata(unittest.TestCase):
             except: pass
 
     def test_compliance_matrix_generation_v26(self):
-        res = self.tracker.generate_run_summary("run_20260729", 100, 95, meta_dir=self.test_meta_dir)
+        res = self.tracker.generate_run_summary("run_20260807", 100, 95, meta_dir=self.test_meta_dir)
         self.assertTrue(res["compliance_checks"]["anonymization_applied"])
         self.assertEqual(res["compliance_checks"]["schema_version"], "v2.6")
-        self.assertEqual(res["environment_telemetry"]["engine_version"], "2.0.2")
+        self.assertEqual(res["environment_telemetry"]["engine_version"], "2.1.2")
 
-    def test_network_heartbeat_monitored_telemetry_indicator(self):
-        res = self.tracker.generate_run_summary("run_heartbeat_meta_assert", 50, 49, meta_dir=self.test_meta_dir)
+    def test_storage_vfs_allocated_blocks_verified_telemetry_indicator(self):
+        res = self.tracker.generate_run_summary("run_vfs_meta_assert", 80, 79, meta_dir=self.test_meta_dir)
         self.assertIn("resource_profiling", res)
+        self.assertTrue(res["resource_profiling"]["storage_vfs_allocated_blocks_verified"])
         self.assertTrue(res["resource_profiling"]["network_heartbeat_monitored"])
-        self.assertTrue(res["resource_profiling"]["parallel_io_streams_verified"])
 if __name__ == '__main__':
     unittest.main()
