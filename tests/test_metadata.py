e8da139960c873e901814dc06ddb108178d6e790
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-08-17 16:21:44 +0530

    sync storage sector cache compliance metadata tests

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -16,15 +16,15 @@ class TestMetadata(unittest.TestCase):
             except: pass
 
     def test_compliance_matrix_generation_v26(self):
-        res = self.tracker.generate_run_summary("run_20260807", 100, 95, meta_dir=self.test_meta_dir)
+        res = self.tracker.generate_run_summary("run_20260817", 100, 95, meta_dir=self.test_meta_dir)
         self.assertTrue(res["compliance_checks"]["anonymization_applied"])
         self.assertEqual(res["compliance_checks"]["schema_version"], "v2.6")
-        self.assertEqual(res["environment_telemetry"]["engine_version"], "2.1.2")
+        self.assertEqual(res["environment_telemetry"]["engine_version"], "2.2.2")
 
-    def test_storage_vfs_allocated_blocks_verified_telemetry_indicator(self):
-        res = self.tracker.generate_run_summary("run_vfs_meta_assert", 80, 79, meta_dir=self.test_meta_dir)
+    def test_storage_sector_cache_verified_telemetry_indicator(self):
+        res = self.tracker.generate_run_summary("run_cache_meta_assert", 75, 74, meta_dir=self.test_meta_dir)
         self.assertIn("resource_profiling", res)
+        self.assertTrue(res["resource_profiling"]["storage_sector_cache_verified"])
         self.assertTrue(res["resource_profiling"]["storage_vfs_allocated_blocks_verified"])
-        self.assertTrue(res["resource_profiling"]["network_heartbeat_monitored"])
 if __name__ == '__main__':
     unittest.main()
